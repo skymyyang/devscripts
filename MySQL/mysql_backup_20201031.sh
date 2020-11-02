86cd2e15377c0b747106e73950b7bdc780046314
skymyyang <yang-li@live.cn>
2020-11-02 19:56:52 +0800

    update-mysqbackup

--- MySQL/mysql_backup_20201031.sh
+++ MySQL/mysql_backup_20201031.sh
@@ -9,9 +9,9 @@ BACK_DIR="/backup"
 
 function wan_back() {
   for dbname in $(ls /usr/local/mysql/data/ | grep "^ainimei"); do
-    ${MYSQL_BIN}/mysqldump -u${MYSQL_USER} -p${MYSQL_PASS} -B ${dbname} | gzip >${BACK_DIR}/${dbname}.${DATE}.sql.gz >>/tmp/mysqlbackup_succeed.log
+    ${MYSQL_BIN}/mysqldump -u${MYSQL_USER} -p${MYSQL_PASS} -B ${dbname} | gzip >${BACK_DIR}/${dbname}.${DATE}.sql.gz 
     if [ $? -eq 0 ]; then
-      /usr/bin/scp ${BACK_DIR}/${dbname}.${DATE}.sql.gz root@172.16.0.50:/backup >>/tmp/mysqlbackup_succeed.log
+      /usr/bin/scp ${BACK_DIR}/${dbname}.${DATE}.sql.gz root@172.16.0.50:/backup 
       find /backup -name "*.sql.gz" -mtime +30 -delete
 
     else
