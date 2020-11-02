b72cdd4beae1e9462619ba278c71d19e10a07f99
skymyyang <yang-li@live.cn>
2020-11-02 15:36:12 +0800

    new mysqlbackup

---
+++ MySQL/mysql_backup_20201031.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#定义常用变量
+
+MYSQL_USER="root"
+MYSQL_PASS="123456"
+DATE=$(date +"%Y%m%d%H")
+MYSQL_BIN="/usr/local/mysql/bin"
+BACK_DIR="/backup"
+
+function wan_back() {
+  for dbname in $(ls /usr/local/mysql/data/ | grep "^ainimei"); do
+    ${MYSQL_BIN}/mysqldump -u${MYSQL_USER} -p${MYSQL_PASS} -B ${dbname} | gzip >${BACK_DIR}/${dbname}.${DATE}.sql.gz >>/tmp/mysqlbackup_succeed.log
+    if [ $? -eq 0 ]; then
+      /usr/bin/scp ${BACK_DIR}/${dbname}.${DATE}.sql.gz root@172.16.0.50:/backup >>/tmp/mysqlbackup_succeed.log
+      find /backup -name "*.sql.gz" -mtime +30 -delete
+
+    else
+      echo "backup failed" >>/tmp/mysqlbackup_error.log
+    fi
+  done
+
+}
+
+wan_back
