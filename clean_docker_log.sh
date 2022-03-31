7a07d2d7f0f6d792cb25cbc2ebc8370e9c57031b
skymyyang <yang-li@live.cn>
2022-03-31 10:25:48 +0800

    清理docker日志

---
+++ clean_docker_log.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "======== start clean docker containers logs ========"
+logs=$(find /var/lib/docker/containers/ -name *-json.log)
+for log in $logs
+        do
+                echo "clean logs : $log"
+                cat /dev/null > $log
+        done
+echo "======== end clean docker containers logs ========"
